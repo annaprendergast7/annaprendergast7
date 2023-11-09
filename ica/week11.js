3def520bc7b987ff5ea3dd4277f4f7cb5200b980
annaprendergast7 <anna.prendergast@colorado.edu>
2023-11-08 23:49:21 -0700

    yuh

---
+++ ica/week11.js
@@ -0,0 +1,57 @@
+console.log("test");
+
+const x =2;
+const y =2;
+
+
+if (x===y){
+    console.log("SUCCESS");
+}
+else{
+    console.log("FAIL");
+}
+
+const btn = document.querySelector
+("button");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+document.addEventListener
+("DOMContentLoaded", () => {
+    canvas.width = document.
+    documentElement.clientWidth;
+    canvas.height = document.
+    documentElement.clientHeight;
+});
+
+function random(number) {
+    //number is parameter, where giving the code something to work with 
+    return Math.floor (Math.random 
+    () = number);
+}
+
+
+function draw(){
+    //console.log("TEST");
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    for (let i =0; i<100; i++) {
+        ctx.beginPath();
+        let red = random(255);
+        let green = random(255);
+        let blue = random (255);
+        let color = "rgba("+red+","+green+","+blue",0.5)"
+        console.log(color);
+        ctx.fillStyle = "rgba(255,0,0,0.5)"
+        ctx.arc(
+            random(canvas.width),
+            random(canvas.height),
+            random(50),
+            0,
+            2 * Math.PI,
+        );
+        ctx.fill();
+        )
+    }
+}
+
+btn.addEventListener("click",draw);
